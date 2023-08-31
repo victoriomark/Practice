b98d25de2060c4a1415ddbdd085e52ef890fb7e6
Your Name <you@example.com>
2023-09-01 00:27:18 +0800

    update

--- ndex.js
+++ ndex.js
@@ -1,12 +1,16 @@
-const message = document.querySelector('.message');
+
 
 const ClickMO = () => {
+   const message = document.querySelector('.message');
 const sound = new Audio('./sound/sounds.mp3');
-sound.play();
-sound.currentTime = 0;
-}
 
 const btn = document.querySelector('button').addEventListener('click',() => {
-   ClickMO()
    message.style.display ='flex'
+   sound.play();
+   sound.currentTime = 0;
+
 })
+
+}
+
+ClickMO();
